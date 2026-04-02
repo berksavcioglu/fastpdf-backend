2d9f3371ea7442ccc8c2cb21cbc5448aaa5e2760
Berk Savcioglu <savciogluberk@gmail.com>
2026-04-02 19:34:49 +0300

    allow vercel frontend cors

--- Program.cs
+++ Program.cs
@@ -15,7 +15,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins(
+                "http://localhost:5173",
+                "https://fastpdf-frontend.vercel.app"
+            )
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
